262a8ac33d506b51b89930c136b00268d67c2073
aeberharhsr <19borki94@gmail.com>
2022-09-24 21:30:33 +0200

    data cleaning for goalies

--- data_cleaning_goalies.py
+++ data_cleaning_goalies.py
@@ -2,6 +2,14 @@ import os
 import glob
 import pandas as pd
 
-df = pd.read_csv("./raw_data/goalies_2021_240920221718.csv")
+df_all = pd.read_csv("./raw_data/goalies_2021_240920221718.csv")
 
-df.head()
+for column_headers in df_all.columns: 
+    print(column_headers)
+    
+#df = df.drop(columns=['xRebounds', 'xGoals','unblocked_shot_attempts','flurryAdjustedxGoals', 'xFreeze', 'freeze', 'ongoal', 'xPlayStopped', 'playStopped', 'xPlayContinuedInZone', 'playContinuedInZone', 'xPlayContinuedOutsideZone', 'playContinuedOutsideZone', 'lowDangerxGoals', 'mediumDangerxGoals', 'highDangerxGoals', 'penalties', 'unblocked_shot_attempts', 'blocked_shot_attempts'])
+
+df_goalies = df_all[['playerId','season', 'name', 'team', 'position', 'situation', 'games_played', 'icetime', 'goals', 'xGoals', 'rebounds', 'xRebounds', 'ongoal', 'xOnGoal', 'lowDangerShots', 'mediumDangerShots', 'highDangerShots', 'lowDangerxGoals', 'mediumDangerxGoals', 'highDangerxGoals', 'lowDangerGoals', 'mediumDangerGoals', 'highDangerGoals', 'penalityMinutes']]
+
+result = df_goalies.head(10)
+print(result)
